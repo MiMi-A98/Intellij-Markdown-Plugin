881275fb425fabf3fbd5811508739c9573b3a500
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-11-08 16:17:46 +0200

    implemented functionality to detect an open markdown file editor

--- src/main/java/MiMiA98/markdownTemplate/Template.java
+++ src/main/java/MiMiA98/markdownTemplate/Template.java
@@ -2,8 +2,11 @@ package MiMiA98.markdownTemplate;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
@@ -14,6 +17,25 @@ import java.util.stream.Collectors;
 public class Template extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
+
+        Project project = event.getProject();
+        if (project == null) {
+            System.out.println("No project found");
+            return;
+        }
+
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        if (editor == null) {
+            System.out.println("No editor is currently open");
+            return;
+        }
+
+        VirtualFile file = editor.getVirtualFile();
+        if (file == null || !(file.getName().endsWith(".md") || file.getName().endsWith(".markdown"))) {
+            System.out.println("The active file is not a markdown file");
+            return;
+        }
+
         String textToInsert;
         try {
             textToInsert = readTemplateText();
