09197d9bd775409a21b2c627dc67baa210aed414
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-11-08 16:39:02 +0200

    implemented logic to read text from active markdown file editor

--- src/main/java/MiMiA98/markdownPlugin/TableOfContents.java
+++ src/main/java/MiMiA98/markdownPlugin/TableOfContents.java
@@ -1,4 +1,37 @@
 package MiMiA98.markdownPlugin;
 
-public class TableOfContents {
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class TableOfContents extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+
+        Project project = event.getProject();
+        if (project == null) {
+            System.out.println("No project found");
+            return;
+        }
+
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        if (editor == null) {
+            System.out.println("No editor is currently open");
+            return;
+        }
+
+        VirtualFile file = editor.getVirtualFile();
+        if (file == null || !(file.getName().endsWith(".md") || file.getName().endsWith(".markdown"))) {
+            System.out.println("The active file is not a markdown file");
+            return;
+        }
+
+        Document document = editor.getDocument();
+        String originalContent = document.getText();
+    }
 }
