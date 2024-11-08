31fca4a75dfd24c4d375b0cba3e53651f8d42d26
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-11-08 16:09:49 +0200

    implemented functionality to read template.md file and shw it in a message dialog box

--- src/main/java/MiMiA98/markdownTemplate/Template.java
+++ src/main/java/MiMiA98/markdownTemplate/Template.java
@@ -2,11 +2,32 @@ package MiMiA98.markdownTemplate;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Template extends AnAction {
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        String textToInsert;
+        try {
+            textToInsert = readTemplateText();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        Messages.showInfoMessage(textToInsert, "Template");
+    }
+
+    private String readTemplateText() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(("/template.md"))))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
 
     }
 }
